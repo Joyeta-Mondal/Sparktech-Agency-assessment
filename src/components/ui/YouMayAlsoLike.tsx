9d4554668a8b50648f58bb17a52d8368111b6c70
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2025-07-02 19:38:56 +0600

    changed padding outside the cards

--- src/components/ui/YouMayAlsoLike.tsx
+++ src/components/ui/YouMayAlsoLike.tsx
@@ -182,7 +182,7 @@ const YouMayAlsoLike: React.FC = () => {
   const renderPropertyCard = (property: Property) => (
     <Card
       key={property.id}
-      className="w-[326px] h-[455px] sm:px-2 p-0 hover:shadow-xl transition-all duration-300 hover:-translate-y-1 flex-shrink-0"
+      className="w-[326px] h-[455px] sm:mx-2 p-0 hover:shadow-xl transition-all duration-300 hover:-translate-y-1 flex-shrink-0"
     >
       <CardContent className="p-0 pb-3 flex flex-col h-full">
         <div className="relative w-full h-[243px] overflow-hidden rounded-t-lg">
